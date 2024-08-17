3b4fe04e72d0b704b1af43abfc47a0b2aaf2874a
amneshpal <amneshpal016@gmail.com>
2024-08-17 19:30:39 +0530

    Changed the html for the site add header footer

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # path('', views.home, name='home'),
+    path('header', views.header, name='header'),
+    path('footer', views.footer, name='footer'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def home(request):
+    return render(request, 'blog/home.html', {})
+
+
+def header(request):
+    return render(request, 'blog/header.html', {})
+
+def footer(request):
+    return render(request, 'blog/footer.html', {})
+
